2f55544297359a624969c55750438f31cc71a0e0
techexpert65-19894 <u_ku_su_tu_nu@hotmail.co.jp>
2020-03-16 21:29:54 +0900

    Update application_controller.rb
    デプロイ調整
    
    多分、jsなりcssなりの参照部分で@assetsを頭につけないといけないっぽいが
    その先の対応の仕方がよく分からないのでとりあえずどうなるのか実行してみる。

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 class ApplicationController < ActionController::Base
+  before_action :set_assets
   protect_from_forgery with: :exception
   
   # ログイン済ユーザーのみアクセスを許可する。
@@ -6,6 +7,12 @@ class ApplicationController < ActionController::Base
   before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
 
+  private
+
+  def set_assets
+    @assets = Rails.application.assets || ::Sprockets::Railtie.build_environment(Rails.application)
+  end
+  
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname])
   end
