3b41f26c6ad5e072a32a03dbcfa568ef7d890e71
techexpert65-19894 <u_ku_su_tu_nu@hotmail.co.jp>
2020-05-13 12:50:20 +0900

    修正したいdeviseコントローラのルーティングを設定
    保存してあるタイピング結果resultの保存を組み込む
    コントローラへのルーティングを設定する

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,16 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: {
+    registrations: 'users/registrations',
+    sessions: 'users/sessions'
+  }
   root to: 'questions#index'
+  
   resources :questions do
     member do
       get 'play'
       post 'result', defaults: { format: 'json' }
     end
   end
+  
   resources :rankings
 end
