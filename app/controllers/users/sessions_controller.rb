9ca55a31a8a15b84e06a1a69aa35304637b7a04b
techexpert65-19894 <u_ku_su_tu_nu@hotmail.co.jp>
2020-05-15 10:20:47 +0900

    ゲストログインにタイピング結果の登録を追加
    ゲストログインにタイピング結果の登録を追加

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -11,32 +11,14 @@ class Users::SessionsController < Devise::SessionsController
   def new_guest
     user = User.guest
     sign_in user
+    result(user.id)
     redirect_to root_path, notice: 'ゲストユーザーとしてログインしました。'
   end
 
   # POST /resource/sign_in
   def create
-    self.resource = warden.authenticate!(auth_options)
-    set_flash_message!(:notice, :signed_in)
     sign_in(resource_name, resource)
-    
-    # ログインしないでタイピングしていた場合に、キャッシュしておいた
-    # タイピング結果をログイン時に登録する。（新規登録時にもsign_inが走るので
-    # registrations_controllerは触る必要ない）
-    if session[:result] != nil
-      # cookieの有効期限が切れていなければ登録
-      if session[:result_expires_at] > Time.now
-        @result = Result.new(session[:result])
-        @result.user_id = resource.id
-        @result.save
-      end
-      # cookieの破棄
-      session[:result] = nil
-      session[:result_expires_at] = nil
-    end
-    
-    yield resource if block_given?
-    respond_with resource, location: after_sign_in_path_for(resource)
+    result(resource.id)
   end
 
   # DELETE /resource/sign_out
@@ -44,8 +26,25 @@ class Users::SessionsController < Devise::SessionsController
   #   super
   # end
 
-  # protected
+  protected
 
+  # ログインしないでタイピングしていた場合に、キャッシュしておいた
+  # タイピング結果をログイン時に登録する。（新規登録時にもsign_inが走るので
+  # registrations_controllerは触る必要ない）
+  def result(userID)
+    if session[:result] != nil
+      # cookieの有効期限が切れていなければ登録
+      if session[:result_expires_at] > Time.now
+        @result = Result.new(session[:result])
+        @result.user_id = userID
+        @result.save
+      end
+      # cookieの破棄
+      session[:result] = nil
+      session[:result_expires_at] = nil
+    end
+  end
+  
   # If you have extra params to permit, append them to the sanitizer.
   # def configure_sign_in_params
   #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
