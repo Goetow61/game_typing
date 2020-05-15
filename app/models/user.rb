3be440fe84c4e33e1e2ced080430fd7ddf7cd882
techexpert65-19894 <u_ku_su_tu_nu@hotmail.co.jp>
2020-05-15 09:38:43 +0900

    ゲストログインの実装
    settion_controllerにnew_guestアクションを追加。
    ルーティングにnew_guestアクションを追加。
    ヘッダにゲストログインボタンを追加。
    Userモデルにゲストユーザ検索及び追加処理を記述。

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,12 @@ class User < ApplicationRecord
 
   has_many :qfiles, dependent: :destroy
   has_many :results, dependent: :destroy
+
+  def self.guest
+    find_or_create_by!(email: 'guest@guest') do |user|
+      user.password = SecureRandom.urlsafe_base64
+      user.nickname = 'ゲスト'
+    end
+  end
+
 end
