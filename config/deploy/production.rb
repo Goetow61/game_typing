d23f45a3c994d74a766199b2a7f087ea14272592
techexpert65-19894 <u_ku_su_tu_nu@hotmail.co.jp>
2020-06-20 22:35:19 +0900

    Update production.rb
    update production.rb

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -5,7 +5,7 @@
 
 # server "example.com", user: "deploy", roles: %w{app db web}, my_property: :my_value
 # server "example.com", user: "deploy", roles: %w{app web}, other_property: :other_value
-server "13.115.255.166", user: "ec2-user", roles: %w{app db web}
+server "game-typing.work", user: "ec2-user", roles: %w{app db web}
 
 
 
